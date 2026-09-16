ccbf711f8bf293b21ca1e7beacc6428b3a975e8b
maoxudong1991 <maoxudong0813@163.com>
2026-09-16 15:02:05 +0800

    test: add executable milestone release profile checks

---
+++ tests/release-readiness/check_release_profile.cmake
@@ -0,0 +1,20 @@
+cmake_minimum_required(VERSION 3.21)
+
+foreach(required_var HYREMOTE_SOURCE_DIR HYREMOTE_TEST_VERSION HYREMOTE_TEST_QML HYREMOTE_TEST_QPA)
+    if(NOT DEFINED ${required_var})
+        message(FATAL_ERROR "${required_var} is required")
+    endif()
+endforeach()
+
+list(APPEND CMAKE_MODULE_PATH "${HYREMOTE_SOURCE_DIR}/cmake")
+include(HyRemoteReleaseProfile)
+
+hyremote_validate_release_profile(
+    VERSION "${HYREMOTE_TEST_VERSION}"
+    QML_ENABLED "${HYREMOTE_TEST_QML}"
+    QPA_ENABLED "${HYREMOTE_TEST_QPA}"
+)
+
+message(STATUS
+    "HyRemote release-profile check passed: version=${HYREMOTE_TEST_VERSION}, "
+    "qml=${HYREMOTE_TEST_QML}, qpa=${HYREMOTE_TEST_QPA}")
